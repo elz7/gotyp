dc22b59c55b6d2750a1f663e72e74a1b6f17a074
elz7 <abtyrev@yandex.ru>
2022-07-20 01:42:34 +0300

    Add menu and debug

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/awesome-gocui/gocui"
 	"github.com/fatih/color"
+)
+
+const (
+	MenuView         = "menu-view"
+	DebugConsoleView = "debug-console-view"
+	DebugPromptView  = "debug-prompt-view"
 )
 
 func main() {
@@ -38,6 +45,44 @@ func layout(g *gocui.Gui) error {
 		g := color.New(color.FgGreen)
 		g.Fprint(v, "Hello World")
 	}
+
+	if v, err := g.SetView(DebugConsoleView, 1, 1, maxX-1, maxY-5, 0); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Debug"
+		v.Frame = true
+		v.Autoscroll = true
+
+		g.SetViewOnBottom(DebugConsoleView)
+	}
+
+	if v, err := g.SetView(DebugPromptView, 1, maxY-4, maxX-1, maxY-1, 0); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Editable = true
+		v.Frame = true
+
+		g.SetViewOnBottom(DebugPromptView)
+	}
+
+	if v, err := g.SetView(MenuView, maxX/2-11, maxY/2-2, maxX/2+11, maxY/2+2, 0); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Menu"
+		v.Frame = true
+		v.SelFgColor = gocui.ColorGreen
+		v.Highlight = true
+
+		fmt.Fprintf(v, "1. Play\n")
+		fmt.Fprintf(v, "2. Settings\n")
+		fmt.Fprintf(v, "3. Quit")
+
+		g.SetCurrentView(MenuView)
+	}
+
 	return nil
 }
 
@@ -50,7 +95,9 @@ func setKeybindings(g *gocui.Gui) error {
 		return true
 	}
 
-	if f("", gocui.KeyCtrlC, gocui.ModNone, quit) {
+	if f("", gocui.KeyCtrlC, gocui.ModNone, quit) &&
+		f(MenuView, gocui.KeyArrowUp, gocui.ModNone, cursorUp) && f(MenuView, gocui.KeyArrowDown, gocui.ModNone, cursorDown) &&
+		f(MenuView, gocui.KeyEnter, gocui.ModNone, selectMenuItem) && f(MenuView, gocui.KeySpace, gocui.ModNone, selectMenuItem) {
 		return nil
 	}
 
@@ -59,4 +106,33 @@ func setKeybindings(g *gocui.Gui) error {
 
 func quit(*gocui.Gui, *gocui.View) error {
 	return gocui.ErrQuit
+}
+
+func cursorUp(g *gocui.Gui, v *gocui.View) error {
+	_, cy := v.Cursor()
+	if err := v.SetCursor(0, cy-1); err != nil {
+		v.SetCursor(0, len(v.BufferLines())-1)
+	}
+	return nil
+}
+
+func cursorDown(g *gocui.Gui, v *gocui.View) error {
+	_, cy := v.Cursor()
+	cy = (cy + 1) % len(v.BufferLines())
+	v.SetCursor(0, cy)
+	return nil
+}
+
+func selectMenuItem(g *gocui.Gui, v *gocui.View) error {
+
+	switch _, i := v.Cursor(); i {
+	case 0:
+		return nil
+	case 1:
+		return nil
+	case 2:
+		return quit(g, v)
+	}
+
+	return nil
 }
